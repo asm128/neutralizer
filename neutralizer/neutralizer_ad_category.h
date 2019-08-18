1cf30e30305acf1369d27a5a8de61181a7029694
asm128 <pabloarielz@outlook.com>
2019-08-18 04:56:12 -0300

    + Added htmlBoardGenerate().

--- neutralizer/neutralizer_ad_category.h
+++ neutralizer/neutralizer_ad_category.h
@@ -10,6 +10,7 @@ namespace ntl
 	GDEFINE_ENUM_VALUE(AD_SHOP_CATEGORY, shops, 2);
 	GDEFINE_ENUM_VALUE(AD_SHOP_CATEGORY, shows, 3);
 
+	::gpk::error_t								htmlBoardGenerate				(const ::gpk::view_const_string & contentFileName, const AD_SHOP_CATEGORY category, const ::gpk::view_const_string & title, const ::gpk::view_const_string & lang, ::gpk::array_pod<char_t> & output);
 } // namespace
 
 #endif // NEUTRALIZER_AD_CATEGORY_H_239084293847
