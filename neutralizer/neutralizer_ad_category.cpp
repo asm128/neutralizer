eef0508812320eaa5f154b1b7f95715a7e506c91
asm128 <pabloarielz@outlook.com>
2019-08-21 00:57:46 -0300

    + Removed nonsense redundancies.

--- neutralizer/neutralizer_ad_category.cpp
+++ neutralizer/neutralizer_ad_category.cpp
@@ -31,6 +31,16 @@
 	return 0;
 }
 
+struct SItemViews {
+	::gpk::view_const_string					URL							= {};
+	::gpk::view_const_string					Title						= {};
+	::gpk::view_const_string					Text						= {};
+	::gpk::view_const_string					ImageHRef					= {};
+	::gpk::view_const_string					ImageSrc					= {};
+	::gpk::view_const_string					ImageTitle					= {};
+	::gpk::view_const_string					ImageAlt					= {};
+	::gpk::view_const_string					MapURL						= {};
+};
 
 ::gpk::error_t								ntl::htmlBoardGenerate				(const ::gpk::view_const_string & contentFileName, const ::ntl::AD_SHOP_CATEGORY category, const ::gpk::view_const_string & title, const ::gpk::view_const_string & lang, ::gpk::array_pod<char_t> & output)	{
 	output.append(::gpk::view_const_string{ "\n<table style=\"width:100%;height:100%;text-align:center;\">"});
@@ -45,12 +55,12 @@
 	output.append(::gpk::view_const_string{ "\n<tr style=\"\" >"});
 	output.append(::gpk::view_const_string{ "\n<td style=\"width:100%;font-size:24px; font-weight:bold; vertical-align:top;\">"});
 
-
 	//---------------------
 	::gpk::SJSONFile										config							= {};
 	gpk_necall(::gpk::jsonFileRead(config, contentFileName), "Failed to load configuration file: %s.", contentFileName);
-	const ::gpk::error_t									countItems						= ::gpk::jsonArraySize(*config.Reader[0]);
-	for(int32_t iItem = 0; iItem < countItems; ++iItem) {
+	SItemViews												views;
+	::gpk::array_pod<uint32_t>								indicesToDisplay				= {};
+	for(int32_t iItem = 0, countItems = ::gpk::jsonArraySize(*config.Reader[0]); iItem < countItems; ++iItem) {
 		const ::gpk::error_t									jsonIndexCurrentItem			= ::gpk::jsonArrayValueGet(*config.Reader[0], iItem);
 		::gpk::view_const_string								viewWikiCategory				= {};
 		const ::gpk::error_t									jsonIndexArrayCategory			= ::gpk::jsonExpressionResolve("category"	, config.Reader, jsonIndexCurrentItem, viewWikiCategory);
@@ -65,51 +75,40 @@
 		}
 		if(false == isCategory)
 			continue;
+		gpk_necall(indicesToDisplay.push_back(iItem), "%s", "Out of memory?");
+	}
 
-		::gpk::view_const_string								viewWikiURL						= {};
-		::gpk::view_const_string								viewWikiTitle					= {};
-		::gpk::view_const_string								viewWikiText					= {};
-		::gpk::view_const_string								viewWikiImageHRef				= {};
-		::gpk::view_const_string								viewWikiImageSrc				= {};
-		::gpk::view_const_string								viewWikiImageTitle				= {};
-		::gpk::view_const_string								viewWikiImageAlt				= {};
-		::gpk::view_const_string								viewMapURL						= {};
-		const ::gpk::error_t									jsonIndexArrayPhone				= ::gpk::jsonExpressionResolve("phone"		, config.Reader, jsonIndexCurrentItem, viewMapURL);
-		const ::gpk::error_t									jsonIndexArrayWP				= ::gpk::jsonExpressionResolve("whatsapp"	, config.Reader, jsonIndexCurrentItem, viewMapURL);
-		const ::gpk::error_t									jsonIndexArrayAddr				= ::gpk::jsonExpressionResolve("address"	, config.Reader, jsonIndexCurrentItem, viewMapURL);
-		const ::gpk::error_t									jsonIndexMap					= ::gpk::jsonExpressionResolve("location"	, config.Reader, jsonIndexCurrentItem, viewMapURL);
-		if(lang == ::gpk::view_const_string{"es"}) {
-			const ::gpk::error_t									jsonIndexWiki					= ::gpk::jsonExpressionResolve("es.wiki"		, config.Reader, jsonIndexCurrentItem, viewWikiURL			);
-			const ::gpk::error_t									jsonIndexName					= ::gpk::jsonExpressionResolve("es.title"		, config.Reader, jsonIndexCurrentItem, viewWikiTitle		);
-			const ::gpk::error_t									jsonIndexText					= ::gpk::jsonExpressionResolve("es.text"		, config.Reader, jsonIndexCurrentItem, viewWikiText			);
-			const ::gpk::error_t									jsonIndexImageHRef				= ::gpk::jsonExpressionResolve("es.image.href"	, config.Reader, jsonIndexCurrentItem, viewWikiImageHRef	);
-			const ::gpk::error_t									jsonIndexImageSrc				= ::gpk::jsonExpressionResolve("es.image.src"	, config.Reader, jsonIndexCurrentItem, viewWikiImageSrc		);
-			const ::gpk::error_t									jsonIndexImageTitle				= ::gpk::jsonExpressionResolve("es.image.title"	, config.Reader, jsonIndexCurrentItem, viewWikiImageTitle	);
-			const ::gpk::error_t									jsonIndexImageAlt				= ::gpk::jsonExpressionResolve("es.image.alt"	, config.Reader, jsonIndexCurrentItem, viewWikiImageAlt		);
-		}
-		else {
-			const ::gpk::error_t									jsonIndexWiki					= ::gpk::jsonExpressionResolve("en.wiki"		, config.Reader, jsonIndexCurrentItem, viewWikiURL			);
-			const ::gpk::error_t									jsonIndexName					= ::gpk::jsonExpressionResolve("en.title"		, config.Reader, jsonIndexCurrentItem, viewWikiTitle		);
-			const ::gpk::error_t									jsonIndexText					= ::gpk::jsonExpressionResolve("en.text"		, config.Reader, jsonIndexCurrentItem, viewWikiText			);
-			const ::gpk::error_t									jsonIndexImageHRef				= ::gpk::jsonExpressionResolve("en.image.href"	, config.Reader, jsonIndexCurrentItem, viewWikiImageHRef	);
-			const ::gpk::error_t									jsonIndexImageSrc				= ::gpk::jsonExpressionResolve("en.image.src"	, config.Reader, jsonIndexCurrentItem, viewWikiImageSrc		);
-			const ::gpk::error_t									jsonIndexImageTitle				= ::gpk::jsonExpressionResolve("en.image.title"	, config.Reader, jsonIndexCurrentItem, viewWikiImageTitle	);
-			const ::gpk::error_t									jsonIndexImageAlt				= ::gpk::jsonExpressionResolve("en.image.alt"	, config.Reader, jsonIndexCurrentItem, viewWikiImageAlt		);
-		}
-		output.append(::gpk::view_const_string{ "\n<table style=\"width:100%;text-align:center;border-style:solid;border-width:2px;\" >"});
-		output.append(::gpk::view_const_string{ "\n<tr>"});
+	output.append(::gpk::view_const_string{ "\n<table style=\"width:100%;text-align:center;border-style:solid;border-width:2px;\" >"});
+	for(int32_t iItem = 0, countItems = indicesToDisplay.size(); iItem < countItems; ++iItem) {
+		const ::gpk::error_t									jsonIndexCurrentItem			= ::gpk::jsonArrayValueGet(*config.Reader[0], indicesToDisplay[iItem]);
+		const ::gpk::error_t									jsonIndexArrayPhone				= ::gpk::jsonExpressionResolve("phone"		, config.Reader, jsonIndexCurrentItem, views.MapURL		);
+		const ::gpk::error_t									jsonIndexArrayWP				= ::gpk::jsonExpressionResolve("whatsapp"	, config.Reader, jsonIndexCurrentItem, views.MapURL		);
+		const ::gpk::error_t									jsonIndexArrayAddr				= ::gpk::jsonExpressionResolve("address"	, config.Reader, jsonIndexCurrentItem, views.MapURL		);
+		const ::gpk::error_t									jsonIndexMap					= ::gpk::jsonExpressionResolve("location"	, config.Reader, jsonIndexCurrentItem, views.MapURL		);
+		// ---- Language-based properties.
+		const ::gpk::error_t									jsonIndexLang					= ::gpk::jsonExpressionResolve(lang			, config.Reader, jsonIndexCurrentItem, views.URL		);
+		const ::gpk::error_t									jsonIndexWiki					= ::gpk::jsonExpressionResolve("wiki"		, config.Reader, jsonIndexLang, views.URL		);
+		const ::gpk::error_t									jsonIndexName					= ::gpk::jsonExpressionResolve("title"		, config.Reader, jsonIndexLang, views.Title		);
+		const ::gpk::error_t									jsonIndexText					= ::gpk::jsonExpressionResolve("text"		, config.Reader, jsonIndexLang, views.Text		);
+		const ::gpk::error_t									jsonIndexImageHRef				= ::gpk::jsonExpressionResolve("image.href"	, config.Reader, jsonIndexLang, views.ImageHRef	);
+		const ::gpk::error_t									jsonIndexImageSrc				= ::gpk::jsonExpressionResolve("image.src"	, config.Reader, jsonIndexLang, views.ImageSrc	);
+		const ::gpk::error_t									jsonIndexImageTitle				= ::gpk::jsonExpressionResolve("image.title", config.Reader, jsonIndexLang, views.ImageTitle	);
+		const ::gpk::error_t									jsonIndexImageAlt				= ::gpk::jsonExpressionResolve("image.alt"	, config.Reader, jsonIndexLang, views.ImageAlt	);
+		output.append(::gpk::view_const_string{ "\n<tr id=\""});
+		output.append();
+		output.append(::gpk::view_const_string{ "\">"});
 		output.append(::gpk::view_const_string{ "\n<td style=\"width:100%;text-align:left;font-size:32px;vertical-align:top;\">"});
 		//
 			output.append(::gpk::view_const_string{ "\n<table style=\"width:100%;height:100%;text-align:center;border-style:solid;border-width:2px;border-radius:16px;\" >"});
 			output.append(::gpk::view_const_string{ "\n<tr>"});
 			output.append(::gpk::view_const_string{ "\n<td style=\"background-color:lightgrey;text-align:left;font-size:32px;vertical-align:top;\">"});
 			//
-			::ntl::htmlHRefLink(viewWikiTitle, viewWikiURL, "style=\"text-align:left;text-decoration:none;font-weight:bold;\"", output);
+			::ntl::htmlHRefLink(views.Title, views.URL, "style=\"text-align:left;text-decoration:none;font-weight:bold;\"", output);
 			output.append(::gpk::view_const_string{"\n</td>"});
 			output.append(::gpk::view_const_string{"\n</tr>"});
 			output.append(::gpk::view_const_string{ "\n<tr>"});
 			output.append(::gpk::view_const_string{ "\n<td style=\"background-color:white;height:100%;text-align:left;font-size:32px;vertical-align:top;\">"});
-			::ntl::htmlTag("p", viewWikiText, "style=\" font-weight:normal;text-align:left;\"", output);
+			::ntl::htmlTag("p", views.Text, "style=\" font-weight:normal;text-align:left;\"", output);
 			//
 			output.append(::gpk::view_const_string{"\n</td>"});
 			output.append(::gpk::view_const_string{"\n</tr>"});
@@ -122,13 +121,13 @@
 			output.append(::gpk::view_const_string{ "\n<tr>"});
 			output.append(::gpk::view_const_string{ "\n<td style=\"background-color:lightgrey;text-align:left;font-size:32px;vertical-align:top;width:100%;\">"});
 			output.append(::gpk::view_const_string{ "<a target=\"blank\" title=\""});
-			output.append(viewWikiImageTitle);
+			output.append(views.ImageTitle);
 			output.append(::gpk::view_const_string{ "\" href=\""});
-			output.append(viewWikiImageHRef);
+			output.append(views.ImageHRef);
 			output.append(::gpk::view_const_string{ "\" class=\"image\" ><img style=\"width:320px;\" decoding=\"async\" elementtiming=\"thumbnail\" alt=\""});
-			output.append(viewWikiImageAlt);
+			output.append(views.ImageAlt);
 			output.append(::gpk::view_const_string{ "\" src=\""});
-			output.append(viewWikiImageSrc);
+			output.append(views.ImageSrc);
 			output.append(::gpk::view_const_string{ "\" ></a>"});
 			output.append(::gpk::view_const_string{"\n</td>"});
 			output.append(::gpk::view_const_string{"\n</tr>"});
@@ -138,10 +137,10 @@
 			// ----- Image info table
 				output.append(::gpk::view_const_string{ "\n<table style=\"width:100%;height:100%;text-align:center;border-style:solid;border-width:2px;border-radius:16px;\" >"});
 				output.append(::gpk::view_const_string{ "\n<tr>"});
-				if(viewMapURL.size()) {
+				if(views.MapURL.size()) {
 					output.append(::gpk::view_const_string{ "\n<td style=\"background-color:lightgrey;text-align:center;font-size:32px;vertical-align:top;\">"});
 					output.append(::gpk::view_const_string{"\n<a target=\"blank\" style=\"margin:4px;font-size:24px;\" href=\""});
-					output.append(viewMapURL);
+					output.append(views.MapURL);
 					output.append(::gpk::view_const_string{"\">"});
 					if(lang == ::gpk::view_const_string{"es"})
 						output.append(::gpk::view_const_string{"Localización"});
@@ -156,7 +155,6 @@
 				// ----- Contact info
 				const ::gpk::error_t									countAddr						= ::gpk::jsonArraySize(*config.Reader[jsonIndexArrayAddr	]);
 				for(int32_t iPhone = 0; iPhone < countAddr	; ++iPhone) { ::gpk::view_const_string textPhone = config.Reader.View[::gpk::jsonArrayValueGet(*config.Reader[jsonIndexArrayAddr	], iPhone)]; output.append(textPhone); output.append(::gpk::view_const_string{"\n<br />"}); }
-
 
 				//
 				output.append(::gpk::view_const_string{"\n</td>"});
@@ -191,8 +189,9 @@
 
 		output.append(::gpk::view_const_string{"\n</td>"});
 		output.append(::gpk::view_const_string{"\n</tr>"});
-		output.append(::gpk::view_const_string{"\n</table>"});
 	}
+	output.append(::gpk::view_const_string{"\n</table>"});
+
 	output.append(::gpk::view_const_string{"\n</td>"});
 	output.append(::gpk::view_const_string{"\n</tr>"});
 
